97ffd754f6b0ef91ea84fdd6c8873e6236ad6563
Reid Wilbur <reid.wilbur@gmail.com>
2016-12-15 14:30:57 -0500

    Day14 slns

---
+++ src/main/java/aoc/Day14.java
@@ -0,0 +1,98 @@
+package aoc;
+
+import com.google.common.base.Throwables;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Function;
+
+public class Day14 {
+  private final String salt;
+
+  public Day14(String salt) {
+    this.salt = salt;
+  }
+
+  protected String gethash(int idx) {
+    String idxStr = salt + Integer.toString(idx);
+    return DigestUtils.md5Hex(idxStr.getBytes());
+  }
+
+  protected String getStretchedHash(int idx) {
+    String hash = DigestUtils.md5Hex(salt + Integer.toString(idx));
+    for (int i = 0; i < 2016; i++) {
+      hash = DigestUtils.md5Hex(hash);
+    }
+    return hash;
+  }
+
+  protected Optional<Character> hasTriple(String hash) {
+    for (int idx = 0; idx <= (hash.length() - 3); idx++) {
+      char c = hash.charAt(idx);
+      if (hash.charAt(idx + 1) == c && hash.charAt(idx + 2) == c) {
+        return Optional.of(c);
+      }
+    }
+    return Optional.empty();
+  }
+
+  protected boolean hasQuint(char c, String hash) {
+    for (int idx = 0; idx <= (hash.length() - 5); idx++) {
+      if (hash.charAt(idx) == c
+          && hash.charAt(idx + 1) == c
+          && hash.charAt(idx + 2) == c
+          && hash.charAt(idx + 3) == c
+          && hash.charAt(idx + 4) == c) {
+       return true;
+      }
+    }
+    return false;
+  }
+
+  public List<Map.Entry<Integer,String>> getPads(int startIdx, int size) throws ExecutionException {
+    return getPads(this::gethash, startIdx, size);
+  }
+
+  public List<Map.Entry<Integer,String>> getPads(Function<Integer,String> hashFn, int startIdx, int size) throws ExecutionException {
+    LoadingCache<Integer, String> hashCache = CacheBuilder.newBuilder()
+        .build(
+            new CacheLoader<Integer, String>() {
+              @Override
+              public String load(Integer idx) throws Exception {
+                return hashFn.apply(idx);
+              }
+            }
+        );
+
+    List<Map.Entry<Integer,String>> pads = new ArrayList<>();
+
+    for (int idx = startIdx; pads.size() < size; idx++) {
+      int lidx = idx;
+      String hash = hashFn.apply(lidx);
+      hasTriple(hash).ifPresent(
+          c -> {
+            for (int qidx = lidx + 1; qidx < lidx + 1000; qidx++) {
+              try {
+                String qhash = hashCache.get(qidx);
+                if (hasQuint(c, qhash)) {
+                  pads.add(new AbstractMap.SimpleEntry<>(lidx, hash));
+                }
+              } catch (Exception e) {
+                throw Throwables.propagate(e);
+              }
+            }
+          }
+      );
+    }
+
+    return pads;
+  }
+}
---
+++ src/test/java/aoc/Day14Test.java
@@ -0,0 +1,69 @@
+package aoc;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.AbstractMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class Day14Test {
+
+  private Day14 testInst;
+
+  @Before
+  public void setup() {
+    testInst = new Day14("abc");
+  }
+
+  @Test
+  public void testGetHash() {
+    assertThat(testInst.gethash(18), is("0034e0923cc38887a57bd7b1d4f953df"));
+    assertThat(testInst.gethash(72), is("a213738a53f0dcf7b3033986a9904e11"));
+    assertThat(testInst.gethash(792), is("d3037333338c0e36482facef46a5d375"));
+  }
+
+  @Test
+  public void testHasTriple() {
+    assertThat(testInst.hasTriple(""), is(Optional.empty()));
+    assertThat(testInst.hasTriple("aa"), is(Optional.empty()));
+    assertThat(testInst.hasTriple("aaa"), is(Optional.of('a')));
+    assertThat(testInst.hasTriple("faaabcccd"), is(Optional.of('a')));
+    assertThat(testInst.hasTriple("a213738a53f0dcf7b3033986a9904e11"), is(Optional.empty()));
+  }
+
+  @Test
+  public void hasQuint() {
+    assertThat(testInst.hasQuint('a', ""), is(false));
+    assertThat(testInst.hasQuint('a', "aa"), is(false));
+    assertThat(testInst.hasQuint('a', "aaaa"), is(false));
+    assertThat(testInst.hasQuint('a', "faaaab"), is(false));
+    assertThat(testInst.hasQuint('a', "faaaaab"), is(true));
+    assertThat(testInst.hasQuint('a', "faaaaa"), is(true));
+  }
+
+  @Test
+  public void testGetPads() throws ExecutionException {
+    List<Map.Entry<Integer,String>> pads = testInst.getPads(0, 64);
+    assertThat(pads.get(63), is(new AbstractMap.SimpleEntry<>(22728, "26ccc731a8706e0c4f979aeb341871f0")));
+  }
+
+  @Test
+  public void testGetPadsPart1() throws ExecutionException {
+    Day14 day14 = new Day14("yjdafjpo");
+    List<Map.Entry<Integer,String>> pads = day14.getPads(0, 64);
+    assertThat(pads.get(63), is(new AbstractMap.SimpleEntry<>(25427, "d95b61890f0df9add71f1427ddd7bf54")));
+  }
+
+  @Test
+  public void testGetPadsPart2() throws ExecutionException {
+    Day14 day14 = new Day14("yjdafjpo");
+    List<Map.Entry<Integer,String>> pads = day14.getPads(day14::getStretchedHash,0, 64);
+    assertThat(pads.get(63), is(new AbstractMap.SimpleEntry<>(22045, "e30f959d84b3d585bae8acae233358f9")));
+  }
+}
